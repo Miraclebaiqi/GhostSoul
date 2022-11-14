9dad0119be01d80df7716ae6379510126bf0897b
Miracle_KK <425710382@qq.com>
2022-11-15 02:04:18 +0800

    初始化文件结构

---
+++ Source/GhostSoul/GhostSoulGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GhostSoulGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOSTSOUL_API AGhostSoulGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
